82243096c34787846c2f9de71a81f83ad5343fc6
Chuck Kiefriter <ckiefriter@gmail.com>
2024-03-12 19:38:01 -0400

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO amenity (amenity) VALUES ('Dog friendly');
+INSERT INTO amenity (amenity) VALUES ('cat friendly');
+INSERT INTO amenity (amenity) VALUES ('turlte friendly');
+INSERT INTO amenity (amenity) VALUES ('chicken friendly');
+INSERT INTO amenity (amenity) VALUES ('restrooms');
+INSERT INTO amenity (amenity) VALUES ('Hot dog stand');
---
+++ src/test/java/com/promineotech/week13petpark/Week13PetParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promineotech.week13petpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week13PetParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
